f5d64dc10d5201bec7625a6784c5912f77597bf0
water <1916533559@qq.com>
2019-12-15 22:50:52 +0800

    first commit

---
+++ test2forpro1.java
@@ -0,0 +1,35 @@
+package com.itheima.entity;
+
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+
+import java.util.Map;
+
+@XStreamAlias("xml")
+public class TextMessage extends BaseMessage {
+    @XStreamAlias("Content")
+    private String content;
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public TextMessage(Map<String, String> map, String content) {
+        super(map);
+        this.setMsgType( "text");
+        this.content = content;
+    }
+
+    @Override
+    public String toString() {
+        return "TextMessage{" +
+                "content='" + content + '\'' +
+                getFromUserName()+".."+
+                getToUserName()+".."+
+                getCreateTime()+".."+
+                '}';
+    }
+}
